39a2cb0b3790aeb42336d3293d76570dd178c49e
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-25 16:12:51 -0500

    Clase 6: Documentando la api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express');
+
 const config = require('../config.js');
 const user = require('./components/user/network');
 
@@ -8,8 +10,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const swaggerDoc = require('./swagger.json');
+
 // ROUER
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto ', config.api.port);
