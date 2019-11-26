3737f93009a70b9c3729529fa9db2fa7c1661452
Carlos Hernandez <carloshdmn@gmail.com>
2019-11-26 14:54:12 -0500

    Clase 13: Gestion avanzada de errores

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
+const errors = require('../network/errors');
 
 const app = express();
 
@@ -17,6 +18,8 @@ const swaggerDoc = require('./swagger.json');
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
+app.use(errors);
 
 app.listen(config.api.port, () => {
     console.log('Api escuchando en el puerto ', config.api.port);
--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const error = require('../utils/error');
 
 const secret = config.jwt.secret;
 
@@ -17,18 +18,18 @@ const check = {
         console.log(decoded);
 
         if (decoded.id !== owner) {
-            throw new Error('No puedes hacer esto');
+            throw error('No puedes hacer esto', 401);
         }
     },
 }
 
 function getToken(auth) {
     if (!auth) {
-        throw new Error('No viene token');
+        throw error('No viene token', 401);
     }
 
     if (auth.indexOf('Bearer ') === -1) {
-        throw new Error('Formato invalido');
+        throw error('Formato invalido', 401);
     }
 
     let token = auth.replace('Bearer ', '');
